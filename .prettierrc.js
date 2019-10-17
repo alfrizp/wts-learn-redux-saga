ddace1e714a664f33a7afaf5d36d76ea7e00e739
alip <alifrizkipambudi@gmail.com>
2019-10-17 13:35:12 +0700

    setuping redux

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,5 @@ module.exports = {
     tabWidth: 4,
     singleQuote: true,
     trailingComma: 'all',
+    semi: true
 };
---
+++ src/reducers/imagesReducer.js
@@ -0,0 +1,9 @@
+import { IMAGES } from '../constants';
+
+export default (state = [], action) => {
+    if (action.type === IMAGES.LOAD_SUCCESS) {
+        return [...state, ...action.images];
+    }
+
+    return state;
+}
